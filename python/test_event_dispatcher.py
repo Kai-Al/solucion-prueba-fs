a7c4a5b979bb22fa91f9c7912ed74f9e47870540
Kai-Al <kai.albornoz@gmail.com>
2024-10-24 19:54:07 -0500

    Fix: Camel case to snake case

--- python/test_event_dispatcher.py
+++ python/test_event_dispatcher.py
@@ -1,22 +1,21 @@
 import unittest
 from unittest.mock import MagicMock
-from event_dispatcher import EventDispatcher 
+from event_dispatcher import EventDispatcher
 
 class EventObject:
     pass
 
-test_obj = EventObject()
 class TestEventDispatcher(unittest.TestCase):
 
     def test_mixin(self):
         test_obj = EventObject()
         EventDispatcher.mixin(test_obj)
 
-        self.assertTrue(callable(test_obj.addEventListener))
-        self.assertTrue(callable(test_obj.dispatchEvent))
-        self.assertTrue(callable(test_obj.removeEventListener))
-        self.assertTrue(callable(test_obj.hasListenerFor))
-        self.assertTrue(callable(test_obj.hasCallbackFor))
+        self.assertTrue(callable(test_obj.add_event_listener))
+        self.assertTrue(callable(test_obj.dispatch_event))
+        self.assertTrue(callable(test_obj.remove_event_listener))
+        self.assertTrue(callable(test_obj.has_listener_for))
+        self.assertTrue(callable(test_obj.has_callback_for))
 
     def test_add_event_listener(self):
         test_obj = EventObject()
@@ -24,8 +23,8 @@ class TestEventDispatcher(unittest.TestCase):
         test_event = "testEvent"
         test_func = lambda: print("Test function")
 
-        test_obj.addEventListener(test_event, test_func)
-        self.assertTrue(test_obj.hasListenerFor(test_event))
+        test_obj.add_event_listener(test_event, test_func)
+        self.assertTrue(test_obj.has_listener_for(test_event))
 
     def test_add_event_with_callback(self):
         test_obj = EventObject()
@@ -33,8 +32,8 @@ class TestEventDispatcher(unittest.TestCase):
         test_event = "testEvent"
         test_func = lambda: print("Test function")
 
-        test_obj.addEventListener(test_event, test_func)
-        self.assertTrue(test_obj.hasCallbackFor(test_event, test_func))
+        test_obj.add_event_listener(test_event, test_func)
+        self.assertTrue(test_obj.has_callback_for(test_event, test_func))
 
     def test_multiple_callbacks_for_event(self):
         test_obj = EventObject()
@@ -43,12 +42,12 @@ class TestEventDispatcher(unittest.TestCase):
         test_func = lambda: print("Test function")
         test_func2 = lambda: print("Test function 2")
 
-        test_obj.addEventListener(test_event, test_func)
-        test_obj.addEventListener(test_event, test_func2)
+        test_obj.add_event_listener(test_event, test_func)
+        test_obj.add_event_listener(test_event, test_func2)
 
-        self.assertTrue(test_obj.hasListenerFor(test_event))
-        self.assertTrue(test_obj.hasCallbackFor(test_event, test_func))
-        self.assertTrue(test_obj.hasCallbackFor(test_event, test_func2))
+        self.assertTrue(test_obj.has_listener_for(test_event))
+        self.assertTrue(test_obj.has_callback_for(test_event, test_func))
+        self.assertTrue(test_obj.has_callback_for(test_event, test_func2))
 
     def test_no_shared_store_between_instances(self):
         test_obj = EventObject()
@@ -58,9 +57,9 @@ class TestEventDispatcher(unittest.TestCase):
         test_event = "testEvent"
         test_func = lambda: print("Test function")
 
-        test_obj.addEventListener(test_event, test_func)
-        self.assertTrue(test_obj.hasCallbackFor(test_event, test_func))
-        self.assertFalse(test_obj2.hasCallbackFor(test_event, test_func))
+        test_obj.add_event_listener(test_event, test_func)
+        self.assertTrue(test_obj.has_callback_for(test_event, test_func))
+        self.assertFalse(test_obj2.has_callback_for(test_event, test_func))
 
     def test_has_listener_for(self):
         test_obj = EventObject()
@@ -68,8 +67,8 @@ class TestEventDispatcher(unittest.TestCase):
         test_event = "testEvent"
         test_func = lambda: print("Test function")
 
-        test_obj.addEventListener(test_event, test_func)
-        self.assertFalse(test_obj.hasListenerFor("xxx"))
+        test_obj.add_event_listener(test_event, test_func)
+        self.assertFalse(test_obj.has_listener_for("xxx"))
 
     def test_has_callback_for(self):
         test_obj = EventObject()
@@ -78,9 +77,9 @@ class TestEventDispatcher(unittest.TestCase):
         test_func = lambda: print("Test function")
         test_func2 = lambda: print("Test function 2")
 
-        test_obj.addEventListener(test_event, test_func)
-        self.assertTrue(test_obj.hasCallbackFor(test_event, test_func))
-        self.assertFalse(test_obj.hasCallbackFor(test_event, test_func2))
+        test_obj.add_event_listener(test_event, test_func)
+        self.assertTrue(test_obj.has_callback_for(test_event, test_func))
+        self.assertFalse(test_obj.has_callback_for(test_event, test_func2))
 
     def test_remove_event_listener(self):
         test_obj = EventObject()
@@ -89,12 +88,12 @@ class TestEventDispatcher(unittest.TestCase):
         test_func = lambda: print("Test function")
         test_func2 = lambda: print("Test function 2")
 
-        test_obj.addEventListener(test_event, test_func)
-        test_obj.addEventListener(test_event, test_func2)
-        test_obj.removeEventListener(test_event, test_func)
+        test_obj.add_event_listener(test_event, test_func)
+        test_obj.add_event_listener(test_event, test_func2)
+        test_obj.remove_event_listener(test_event, test_func)
 
-        self.assertTrue(test_obj.hasCallbackFor(test_event, test_func2))
-        self.assertFalse(test_obj.hasCallbackFor(test_event, test_func))
+        self.assertTrue(test_obj.has_callback_for(test_event, test_func2))
+        self.assertFalse(test_obj.has_callback_for(test_event, test_func))
 
     def test_remove_all_callbacks_removes_event_name(self):
         test_obj = EventObject()
@@ -102,10 +101,10 @@ class TestEventDispatcher(unittest.TestCase):
         test_event = "testEvent"
         test_func = lambda: print("Test function")
 
-        test_obj.addEventListener(test_event, test_func)
-        test_obj.removeEventListener(test_event, test_func)
+        test_obj.add_event_listener(test_event, test_func)
+        test_obj.remove_event_listener(test_event, test_func)
 
-        self.assertFalse(test_obj.hasListenerFor(test_event))
+        self.assertFalse(test_obj.has_listener_for(test_event))
 
     def test_dispatch_event(self):
         test_obj = EventObject()
@@ -114,9 +113,9 @@ class TestEventDispatcher(unittest.TestCase):
         test_func = MagicMock()
         test_func2 = MagicMock()
 
-        test_obj.addEventListener(test_event, test_func)
-        test_obj.addEventListener(test_event, test_func2)
-        test_obj.dispatchEvent(test_event)
+        test_obj.add_event_listener(test_event, test_func)
+        test_obj.add_event_listener(test_event, test_func2)
+        test_obj.dispatch_event(test_event)
 
         test_func.assert_called()
         test_func2.assert_called()
@@ -128,10 +127,10 @@ class TestEventDispatcher(unittest.TestCase):
         test_func = MagicMock()
         test_func2 = MagicMock()
 
-        test_obj.addEventListener(test_event, test_func)
-        test_obj.addEventListener(test_event, test_func2)
-        test_obj.removeEventListener(test_event, test_func2)
-        test_obj.dispatchEvent(test_event)
+        test_obj.add_event_listener(test_event, test_func)
+        test_obj.add_event_listener(test_event, test_func2)
+        test_obj.remove_event_listener(test_event, test_func2)
+        test_obj.dispatch_event(test_event)
 
         test_func.assert_called()
         test_func2.assert_not_called()
@@ -147,8 +146,8 @@ class TestEventDispatcher(unittest.TestCase):
             if scope["executeSuccess"]:
                 success["value"] = True
 
-        test_obj.addEventListener(test_event, test_scoped_function, scope)
-        test_obj.dispatchEvent(test_event)
+        test_obj.add_event_listener(test_event, test_scoped_function, scope)
+        test_obj.dispatch_event(test_event)
 
         self.assertTrue(success["value"])
 
